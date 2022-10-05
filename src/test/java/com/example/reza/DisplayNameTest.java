e9f93f0fecd1fa95ceac18b172c8ca7e9a7d5ce9
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-10-05 19:46:12 +0330

    Naming

---
+++ src/test/java/com/example/reza/DisplayNameTest.java
@@ -0,0 +1,23 @@
+package com.example.reza;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("A special test case")
+public class DisplayNameTest {
+
+    @Test
+    @DisplayName("Custom test name containing spaces")
+    void testWithDisplayNameContainingSpaces() {
+    }
+
+    @Test
+    @DisplayName("╯°□°）╯")
+    void testWithDisplayNameContainingSpecialCharacters() {
+    }
+
+    @Test
+    @DisplayName("😱")
+    void testWithDisplayNameContainingEmoji() {
+    }
+}
